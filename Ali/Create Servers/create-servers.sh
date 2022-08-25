d784eaae9c4e521c93cfa2257b01ee89ccf814a9
ali.khan <ali.khan@xgrid.co>
2022-08-25 20:26:09 +0500

    Scripts Updated Ali Create Servers

---
+++ Ali/Create Servers/create-servers.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+ENV_FILE=""
+SSH_CONFIG="~/.ssh/config"
+
+while getopts 'f:' OPTION; do
+  case "$OPTION" in
+  f)
+    ENV_FILE="$OPTARG"
+    if test -f "$ENV_FILE"; then
+      echo "$ENV_FILE exists."
+      source $ENV_FILE
+      #N_SERVER=$N_SERVERS vagrant up
+      if test -f "$SSH_CONFIG"; then
+      #TODO
+      else
+      #TODO
+      fi
+      for i in $(seq 1 $N_SERVERS); do
+        n=`expr 20 + $i` ##https://stackoverflow.com/questions/22460266/linux-bash-for-loop-and-function-for-adding-numbers
+        ip="192.168.56.$n"
+ 
+      done
+    else
+      echo "$ENV_FILE does not exist."
+    fi
+    ;;
+  ?)
+    echo "script usage: create-servers.sh [-f filename]" >&2
+    exit 1
+    ;;
+  esac
+done
+shift "$(($OPTIND - 1))"
