f1d4452eeb2ac3044cc26604448d462d598e4613
ali.khan <ali.khan@xgrid.co>
2022-08-25 21:38:42 +0500

    Updated create server script

--- Ali/Create Servers/create-servers.sh
+++ Ali/Create Servers/create-servers.sh
@@ -4,7 +4,7 @@
 set -e
 
 ENV_FILE=""
-SSH_CONFIG="~/.ssh/config"
+SSH_CONFIG=~/.ssh/config
 
 while getopts 'f:' OPTION; do
   case "$OPTION" in
@@ -13,16 +13,15 @@ while getopts 'f:' OPTION; do
     if test -f "$ENV_FILE"; then
       echo "$ENV_FILE exists."
       source $ENV_FILE
-      #N_SERVER=$N_SERVERS vagrant up
-      if test -f "$SSH_CONFIG"; then
-      #TODO
-      else
-      #TODO
+      N_SERVER=$N_SERVERS vagrant up
+      if ! test -f "$SSH_CONFIG"; then
+        touch $SSH_CONFIG
       fi
+
       for i in $(seq 1 $N_SERVERS); do
         n=`expr 20 + $i` ##https://stackoverflow.com/questions/22460266/linux-bash-for-loop-and-function-for-adding-numbers
         ip="192.168.56.$n"
- 
+        name="server_$n"
       done
     else
       echo "$ENV_FILE does not exist."
